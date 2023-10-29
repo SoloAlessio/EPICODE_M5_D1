aff0a77afaf60dd3491c5618adb0852831c5ed22
Alessio <belcastroalessio@outlook.it>
2023-10-29 15:59:54 +0100

    HERO

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,6 @@
 import { Button, Container, Row, Col } from 'react-bootstrap'
+import ReactLogo from '../logo.svg'
+import GithubLogo from '../githubLogo.svg'
 
 const Welcome = () => (
     <Container className="py-5">
@@ -11,7 +13,7 @@ const Welcome = () => (
                 <div className="lc-block position-relative">
                     <img
                         className="img-fluid"
-                        src="https://static-00.iconduck.com/assets.00/react-illustration-2048x1351-b90lu9jr.png" 
+                        src="https://www.rlogical.com/wp-content/uploads/2020/07/react-js-developer.png" 
                     />
                 </div>
             </Col>
@@ -33,7 +35,31 @@ const Welcome = () => (
                     </div>
                 </div>
                 <div className="lc-block">
-                    <Button variant='secondary p-2' className='btn-lg' role="button">GitHub</Button>
+                    <Button 
+                        href='https://www.react.dev' 
+                        className='mr-3 px-3 border-0 d-inline-flex align-items-center'
+                        style={{backgroundColor:'#f8f9fa', borderRadius: "12px", height: "60px"}}
+                    >
+                        <img 
+                            src={ReactLogo} 
+                            alt="react logo" 
+                            width={"32px"} 
+                            height={"32px"}
+                        />
+                    </Button>
+
+                    <Button 
+                        href='https://github.com/SoloAlessio/EPICODE_M5_D2' 
+                        className='mr-3 px-3 border-0 d-inline-flex align-items-center'
+                        style={{backgroundColor:'#f8f9fa', borderRadius: "12px", height: "60px"}}
+                    >
+                        <img 
+                            src={GithubLogo} 
+                            alt="github logo" 
+                            width={"32px"} 
+                            height={"32px"}
+                        />
+                    </Button>
                 </div>
             </Col>
         </Row>
