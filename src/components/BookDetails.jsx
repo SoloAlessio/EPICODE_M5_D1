4ff74f9206510591896d22a4cc9ef479b5680353
Alessio <belcastroalessio@outlook.it>
2023-11-10 19:12:48 +0100

    jo

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,52 +1,56 @@
 import { React } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import CommentArea from '../components/CommentArea.jsx';
-import fantasy from '../Books/fantasy.json'
-import scifi from '../Books/scifi.json'
-import history from '../Books/history.json'
-import horror from '../Books/horror.json'
-import romance from '../Books/romance.json'
+import fantasy from "../Books/fantasy.json";
+import history from "../Books/history.json";
+import horror from "../Books/horror.json";
+import romance from "../Books/romance.json";
+import scifi from "../Books/scifi.json";
+import CommentArea from "../components/CommentArea.jsx";
 
 export default function BookDetails({ theme }) {
+  const { id, genre } = useParams();
 
-    const { id, genre } = useParams()
+  const BooksByGenre = {
+    fantasy,
+    history,
+    horror,
+    romance,
+    scifi,
+  };
 
-    const BooksByGenre = {
-        fantasy,
-        history,
-        horror,
-        romance,
-        scifi,
-    };
-
-    return (
-        <Container className="my-5">
-            <Row className="g-4">
-                <Col md={8}>
-                    {
-                        BooksByGenre[genre]
-                            .filter((b) => b.asin === id)
-                            .map((b) => (
-                                <Container fluid key={b.asin}>
-                                    <Row className="g-4">
-                                        <Col md={4}>
-                                            <img src={b.img} alt={b.title} className="img-fluid rounded shadow" />
-                                        </Col>
-                                        <Col md={8} style={{color: theme === 'light' ? '#212529' : '#fff'}}>
-                                            <h5>{b.title}</h5>
-                                            <hr />
-                                            <p>$ {b.price}</p>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            ))
-                    }
-                </Col>
-                <Col md={4}>
-                    <CommentArea asin={id}/>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="my-5">
+      <Row className="g-4">
+        <Col md={8}>
+          {BooksByGenre[genre]
+            .filter((b) => b.asin === id)
+            .map((b) => (
+              <Container fluid key={b.asin}>
+                <Row className="g-4">
+                  <Col md={4}>
+                    <img
+                      src={b.img}
+                      alt={b.title}
+                      className="img-fluid rounded shadow"
+                    />
+                  </Col>
+                  <Col
+                    md={8}
+                    style={{ color: theme === "light" ? "#212529" : "#fff" }}
+                  >
+                    <h5>{b.title}</h5>
+                    <hr />
+                    <p>$ {b.price}</p>
+                  </Col>
+                </Row>
+              </Container>
+            ))}
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={id} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
